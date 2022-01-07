98dab414672e93f032e353346fb910f6920e01c0
AudreyFelicio <audreyanwar@gmail.com>
2022-01-07 15:24:20 +0800

    feat: implement navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Homepage from './pages/Homepage';
 import Gamepage from './pages/Gamepage';
+import Test from './pages/Test';
+import Navbar from './components/Navbar';
 
 const Pages = () => {
   return (
     <Routes>
       <Route exact path="/" element={<Homepage />} />
       <Route exact path="/game" element={<Gamepage />} />
+      <Route exact path="/test" element={<Test />} />
     </Routes>
   );
 };
@@ -15,6 +18,7 @@ const Pages = () => {
 const App = () => {
   return (
     <>
+      <Navbar />
       <h1>Singapore Stack Exchange</h1>
       <Pages />
     </>
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,7 +1,39 @@
 import React from 'react';
+import { AppBar, Toolbar, Typography } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import { Link } from 'react-router-dom';
+
+const Wrapper = styled('div')({
+  display: 'flex',
+  alignItems: 'center',
+  width: '100%',
+});
+
+const Space = styled('div')({
+  flexGrow: 1,
+});
+
+const StyledAnchor = styled('a')({
+  color: 'inherit',
+  textDecoration: 'none',
+});
 
 const Navbar = () => {
-  return <h1>TODO!</h1>
+  return (
+    <AppBar elevation={2}>
+      <Toolbar>
+        <Wrapper>
+          <Typography variant="h4">Singapore Stack Exchange</Typography>
+          <Space />
+          <Typography variant="h6">
+            <StyledAnchor href="https://github.com/HackNRoll-Jafar/SingaporeStackExchange" target="_blank" rel="noreferrer">
+              Source
+            </StyledAnchor>
+          </Typography>
+        </Wrapper>
+      </Toolbar>
+    </AppBar>
+  )
 };
 
 export default Navbar;
